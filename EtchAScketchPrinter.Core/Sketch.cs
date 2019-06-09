0a569883d157f930c703852c2874b4e7be87b9af
Paulo Queiroga <queiroga@mmsi.com>
2019-06-09 15:22:31 -0700

    First stabs at creating a sketching model

---
+++ EtchAScketchPrinter.Core/Sketch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace PQ.EtchASketchPrinter.Core
+{
+    public class Sketch
+    {
+        public virtual string Name { get; set; }
+        public IEnumerable<Sketch> Steps { get; set; }
+
+        public virtual IEnumerable<string> Execute(DrawingHands hands)
+        {
+            var steps = new List<string>();
+
+            foreach (var step in Steps)
+            {
+                steps.AddRange(step.Execute(hands));
+            }
+
+            return steps;
+        }
+    }
+}
